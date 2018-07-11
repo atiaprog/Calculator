e8578dd83f518810a2254be28f5a62bf28091853
mohmed atia <atiaprog@gmail.com>
2018-07-11 12:29:17 +0200

    inital Commit

---
+++ src/com/example/java/Calculator.java
@@ -0,0 +1,67 @@
+package com.example.java;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+import static com.example.java.util.MathMethods.*;
+
+public class Calculator {
+
+    public static final String RED = "red";
+
+    public static void main(String[] args) {
+        String string1 = getInput("Enter a Number: ");
+        String string2 = getInput("Enter a Number: ");
+        String operation = getInput("Choose an operation (+ - * /):");
+
+        double result;
+
+        //atia testing the usage and how to make it happend
+        List<String> values=new LinkedList<>();
+
+
+        try {
+            switch (operation) {
+                case "+":
+                    result = addValues(string1, string2);
+                    break;
+                case "-":
+                    result = subtractValues(string1, string2);
+                    break;
+                case "*":
+                    result = multiplyValues(string1, string2);
+                    break;
+                case "/":
+                    result = divideValues(string1, string2);
+                    break;
+                default:
+                    System.out.println("Unrecognized operation!");
+                    return;
+            }
+
+            System.out.println("The answer is " + result);
+
+        } catch (Exception e) {
+            System.out.println("Number formatting exception " + e.getMessage());
+        }
+
+        usColors();
+    }
+
+    private static void usColors() {
+        List<String> colors= new ArrayList<>();
+
+        colors.add(RED);
+        colors.add("blue");
+        colors.add("green");
+    }
+
+    private static String getInput(String prompt) {
+        System.out.print(prompt);
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
+    }
+
+}
